c98bc5f228359f0158e14e2a6f0e0e1671b941af
Ivan Charin <lumidelta07@gmail.com>
2023-02-08 19:46:12 +0300

    Update HW1 with minor fixes

--- HW1.py
+++ HW1.py
@@ -18,6 +18,21 @@ class MplCanvas(FigureCanvas):
         self.axes = fig.add_subplot(111)
         super(MplCanvas, self).__init__(fig)
 
+def addLines(xxdata, xydata):
+    alpha = 0.05
+    n = 49
+    xdata = [xxdata[0]]
+    ydata = [xydata[0]]
+    for i in range(1, len(xxdata)):
+        dx = xxdata[i] - xxdata[i-1]
+        dy = xydata[i] - xydata[i-1]
+        angle = math.atan2(dy, dx)
+        for j in range(1, n):
+            xdata.append(xxdata[i-1] + j * dx/n + alpha * math.sin(angle) if j % 2 == 0 else xxdata[i-1] + j * dx/n - alpha * math.sin(angle))
+            ydata.append(xydata[i-1] + j * dy/n - alpha * math.cos(angle) if j % 2 == 0 else xydata[i-1] + j * dy/n + alpha * math.cos(angle))
+        xdata.append(xxdata[i])
+        ydata.append(xydata[i])
+    return [xdata, ydata]
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -28,17 +43,19 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setCentralWidget(self.canvas)
 
         self.len = 0.1
-        self.dt = 0.01
+        self.dt = 0.1
         self.g = 9.8
         self.k = 500
-        # self.xdata = [0.5, 0.75, 0.5, 0.25, 0.5, 0.75, 0.5, 0.25, 0.5]
-        # self.ydata = [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2]
+        self.xdata = []
+        self.ydata = []
+        self.xxdata = [0.5 for i in range(10, 1, -1)]
+        self.xydata = [i * 0.1 for i in range(10, 1, -1)]
+        self.vxdata = [0 for i in range(100, 0, -1)]
+        self.vydata = [0 for i in range(100, 0, -1)]
+        # self.xxdata = [random.randint(1, 10) * 0.1  for i in range(10, 0, -1)]
+        # self.xydata = [i * 0.1 for i in range(10, 0, -1)]
         # self.vxdata = [0 for i in range(10, 0, -1)]
         # self.vydata = [0 for i in range(10, 0, -1)]
-        self.xdata = [random.randint(1, 10) * 0.1  for i in range(10, 0, -1)]
-        self.ydata = [i * 0.1 for i in range(10, 0, -1)]
-        self.vxdata = [0 for i in range(10, 0, -1)]
-        self.vydata = [0 for i in range(10, 0, -1)]
         self.update_plot()
 
         self.show()
@@ -54,27 +71,25 @@ class MainWindow(QtWidgets.QMainWindow):
         # self.ydata = self.ydata[1:] + [random.randint(0, 10)]
         prev_angle = 0
         prev_len_diff = 0
-        nextx = self.xdata
-        nexty = self.ydata
-        for i in range(len(self.xdata) - 1, 0, -1):
-            if self.ydata[i - 1] == self.ydata[i]:
-                next_angle = 0
-            else:
-                next_angle = math.atan2((self.xdata[i - 1] - self.xdata[i]), (self.ydata[i - 1] - self.ydata[i]))
-            next_len_diff = math.sqrt((self.ydata[i - 1] - self.ydata[i]) ** 2 + (self.xdata[i - 1] - self.xdata[i]) ** 2) - self.len
-            dvx = self.dt * (-prev_len_diff * math.sin(prev_angle) + next_len_diff * math.sin(next_angle)) 
-            dvy = self.dt * (-prev_len_diff * math.cos(prev_angle) + next_len_diff * math.cos(next_angle) - self.g/self.k)
-            self.xdata[i] += (self.vxdata[i] + dvx/2) * self.dt
-            self.ydata[i] += (self.vydata[i] + dvy/2) * self.dt
+        nextx = self.xxdata
+        nexty = self.xydata
+        for i in range(len(self.xxdata) - 1, 0, -1):
+            next_angle = math.atan2((self.xydata[i - 1] - self.xydata[i]), (self.xxdata[i - 1] - self.xxdata[i]))
+            next_len_diff = math.sqrt((self.xydata[i - 1] - self.xydata[i]) ** 2 + (self.xxdata[i - 1] - self.xxdata[i]) ** 2) - self.len
+            dvx = self.dt * (-prev_len_diff * math.cos(prev_angle) + next_len_diff * math.cos(next_angle)) 
+            dvy = self.dt * (-prev_len_diff * math.sin(prev_angle) + next_len_diff * math.sin(next_angle) - self.g/self.k)
+            self.xxdata[i] += (self.vxdata[i] + dvx/2) * self.dt
+            self.xydata[i] += (self.vydata[i] + dvy/2) * self.dt
             self.vxdata[i] += dvx
             self.vydata[i] += dvy
             prev_angle = next_angle
             prev_len_diff = next_len_diff
-        self.xdata = nextx
-        self.ydata = nexty
+        self.xxdata = nextx
+        self.xydata = nexty
+        self.xdata, self.ydata = addLines(self.xxdata, self.xydata)
 
         self.canvas.axes.cla()  # Clear the canvas.
-        self.canvas.axes.plot(self.xdata, self.ydata, 'r')
+        self.canvas.axes.plot(self.xdata, self.ydata, 'r', scalex=False)
         # Trigger the canvas to update and redraw.
         self.canvas.draw()
 
